6f6d49d73cd49dcdc0a91b83b5c9ff113b0d7b18
robdcampbell <rdeancampbell@gmail.com>
2021-03-07 09:03:52 -0500

    Update user routes and controller in the backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,16 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
 // protect middleware will run every time this "profile" route is called
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
