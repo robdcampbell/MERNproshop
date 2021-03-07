1caa12ce2ef5330d0b529be29a3fdad78dcf81f3
robdcampbell <rdeancampbell@gmail.com>
2021-03-06 21:29:10 -0500

    add authMiddleware and functionality in userController, userModel and userRoutes'

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
+// protect middleware will run every time this "profile" route is called
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
