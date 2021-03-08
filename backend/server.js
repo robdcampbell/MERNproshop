a44b2055e097be499eb824cc130cd0494c02a271
robdcampbell <rdeancampbell@gmail.com>
2021-03-08 07:26:31 -0500

    add paypal routes and wire up paypal with OrderScreen.js

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,11 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
+//Paypal API config
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
 // Middleware
 app.use(notFound);
 app.use(errorHandler);
