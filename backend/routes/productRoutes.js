a7235b1d041d8cf836a666d60f588fdb836a52ee
robdcampbell <rdeancampbell@gmail.com>
2021-03-09 17:07:17 -0500

    BackEnd Functionality of adding a review - done

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,10 +7,13 @@ import {
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+
 // Alternate version:
 // router.get("/", getProducts());
 
