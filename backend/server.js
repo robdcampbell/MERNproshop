a13826d2666c07fec76341740c9cb8f6231e8297
robdcampbell <rdeancampbell@gmail.com>
2021-03-09 09:31:24 -0500

    File upload in backend - uploadRoutes.js and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
@@ -6,6 +7,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -19,21 +21,25 @@ app.get("/", (req, res) => {
   res.send("API is running: MERN Proshop...");
 });
 
+// Endpoints: Mount imported routes to these paths
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
-//Paypal API config
+// Paypal API config
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Middleware
 app.use(notFound);
 app.use(errorHandler);
 
 // Custom Error handler that only fires in development mode
-
 const PORT = process.env.PORT || 5000;
 const mode = process.env.NODE_ENV || "Production";
 
