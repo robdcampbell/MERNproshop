c168b7be26dca48fa293c8f63a1afca07e98c6e9
robdcampbell <rdeancampbell@gmail.com>
2021-03-09 23:24:23 -0500

    Add carousel at the products main page

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,12 +8,13 @@ import {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id/reviews").post(protect, createProductReview);
-
+router.get("/top", getTopProducts);
 // Alternate version:
 // router.get("/", getProducts());
 
